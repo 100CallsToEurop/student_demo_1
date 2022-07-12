815085ee23c740277da8b213b22104622c730afd
100CallsToEurop <piaterik123123@mail.ru>
2022-07-12 18:37:22 +0300

    test run

---
+++ src/index.ts
@@ -0,0 +1,232 @@
+import express, {Request, Response} from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
+//Types
+type FieldError = {
+    "message": string,
+    "field": string
+}
+type APIErrorResult = {
+    errorsMessages: Array<FieldError>
+}
+type BloggerInputModel = Omit<BloggerViewModel, 'id'>
+type BloggerViewModel = {
+    id: number,
+    name: string,
+    youtubeUrl: string
+}
+type PostInputModel = Omit<PostViewModel, 'id' | 'bloggerName'>
+type PostViewModel = {
+    id: number
+    title: string,
+    shortDescription: string,
+    content: string,
+    bloggerId: number,
+    bloggerName: string
+}
+
+//Constants
+const parserMiddleware = bodyParser({})
+const port = process.env.PORT || 5000
+const bloggers: Array<BloggerViewModel> = [
+    {id: 1, name: 'About JS - 01', youtubeUrl: 'it-incubator.eu'},
+    {id: 2, name: 'About JS - 02', youtubeUrl: 'it-post.eu'},
+    {id: 3, name: 'About JS - 03', youtubeUrl: 'it-incubator.eu'},
+    {id: 4, name: 'About JS - 04', youtubeUrl: 'it-incubator.eu'},
+    {id: 5, name: 'About JS - 05', youtubeUrl: 'it-incubator.eu'},
+]
+
+const posts: Array<PostViewModel> = [
+    {id: 1, title: 'About JS - 01', shortDescription: 'it-incubator.eu', content: 'it-incubator.eu', bloggerId: 1, bloggerName: 'it-incubator.eu'},
+    {id: 2, title: 'About JS - 02', shortDescription: 'it-incubator.eu', content: 'it-incubator.eu', bloggerId: 2, bloggerName: 'it-incubator.eu'},
+    {id: 3, title: 'About JS - 03', shortDescription: 'it-incubator.eu', content: 'it-incubator.eu', bloggerId: 3, bloggerName: 'it-incubator.eu'},
+    {id: 4, title: 'About JS - 04', shortDescription: 'it-incubator.eu', content: 'it-incubator.eu', bloggerId: 4, bloggerName: 'it-incubator.eu'},
+    {id: 5, title: 'About JS - 05', shortDescription: 'it-incubator.eu', content: 'it-incubator.eu', bloggerId: 5, bloggerName: 'it-incubator.eu'},
+]
+
+
+const app = express()
+
+app.use(cors())
+app.use(parserMiddleware)
+
+//bloggers
+app.get('/bloggers', (req: Request, res: Response)=>{
+    res.status(200).send(bloggers)
+})
+app.get('/bloggers/:id', (req: Request, res: Response) => {
+    const id: number = +req.params.id;
+    const blogger: BloggerViewModel | undefined = bloggers.find(b => b.id === id)
+    if(!blogger) res.status(404).send('Not found')
+    res.status(200).send(blogger)
+})
+app.delete('/bloggers/:id',(req: Request, res: Response)=>{
+    const id: number = +req.params.id;
+    for(let i = 0; i < bloggers.length; i++){
+        if(bloggers[i].id === id) {
+            bloggers.splice(i, 1)
+            res.status(204).send('No Content')
+            return
+        }
+    }
+    res.status(404).send('Not found')
+})
+app.post('/bloggers', (req: Request, res: Response) => {
+    const {name, youtubeUrl }:BloggerInputModel  = req.body
+    if(name && youtubeUrl) {
+        if(name.length > 15 || youtubeUrl.length > 100){
+            const errorMessage: APIErrorResult = {
+                errorsMessages: [{
+                    message: "Field title not found",
+                    field: "title"
+                }]
+            }
+            res.status(400).send(errorMessage)
+        }
+        const newBloger: BloggerViewModel = {
+            id: +(new Date()),
+            name,
+            youtubeUrl
+        }
+        bloggers.push(newBloger)
+        res.status(201).send(newBloger)
+    }
+    else{
+        const errorMessage: APIErrorResult = {
+            errorsMessages: [{
+                message: "Field title not found",
+                field: "title"
+            }]
+        }
+        res.status(400).send(errorMessage)
+    }
+})
+app.put('/bloggers/:id',(req: Request, res: Response)=>{
+    const {name, youtubeUrl }:BloggerInputModel  = req.body
+    if(name && youtubeUrl) {
+        if(name.length > 15 || youtubeUrl.length > 100){
+            const errorMessage: APIErrorResult = {
+                errorsMessages: [{
+                    message: "Field title not found",
+                    field: "title"
+                }]
+            }
+            res.status(400).send(errorMessage)
+        }
+        const id: number = +req.params.id;
+        const blogger: BloggerViewModel | undefined = bloggers.find(b => b.id === id)
+        if (blogger) {
+            blogger.name = name,
+            blogger.youtubeUrl = youtubeUrl
+            res.status(204).send(blogger)
+        }
+        res.status(404).send('NotFound')
+    }
+    else{
+        const errorMessage: APIErrorResult = {
+            errorsMessages: [{
+                message: "Field title not found",
+                field: "title"
+            }]
+        }
+        res.status(400).send(errorMessage)
+    }
+
+})
+
+//posts
+app.get('/posts', (req: Request, res: Response)=>{
+    res.status(200).send(posts)
+})
+
+//400 Bad Request - Доделать
+app.get('/posts/:id', (req: Request, res: Response) => {
+    const id: number = +req.params.id;
+    const post: PostViewModel | undefined = posts.find(p => p.id === id)
+    if(!post) res.status(404).send('Not found')
+    res.status(200).send(post)
+})
+app.delete('/posts/:id',(req: Request, res: Response)=>{
+    const id: number = +req.params.id;
+    for(let i = 0; i < posts.length; i++){
+        if(posts[i].id === id) {
+            posts.splice(i, 1)
+            res.status(204).send('No Content')
+            return
+        }
+    }
+    res.status(404).send('Not found')
+})
+app.post('/posts', (req: Request, res: Response) => {
+    const {title, shortDescription, content, bloggerId }:PostInputModel  = req.body
+    if(title && shortDescription && content && bloggerId) {
+        if(title.length > 30 || shortDescription.length > 100 || content.length > 1000){
+            const errorMessage: APIErrorResult = {
+                errorsMessages: [{
+                    message: "Field title not found",
+                    field: "title"
+                }]
+            }
+            res.status(400).send(errorMessage)
+        }
+        const newPost: PostViewModel = {
+            id: +(new Date()),
+            title,
+            shortDescription,
+            content,
+            bloggerId,
+            bloggerName: bloggers[bloggerId-1].name
+        }
+        posts.push(newPost)
+        res.status(201).send(newPost)
+    }
+    else{
+        const errorMessage: APIErrorResult = {
+            errorsMessages: [{
+                message: "Field title not found",
+                field: "title"
+            }]
+        }
+        res.status(400).send(errorMessage)
+    }
+})
+app.put('/posts/:id',(req: Request, res: Response)=>{
+    const {title, shortDescription, content, bloggerId }:PostInputModel  = req.body
+    if(title && shortDescription && content && bloggerId) {
+        if(title.length > 30 || shortDescription.length > 100 || content.length > 1000){
+            const errorMessage: APIErrorResult = {
+                errorsMessages: [{
+                    message: "Field title not found",
+                    field: "title"
+                }]
+            }
+            res.status(400).send(errorMessage)
+        }
+        const id: number = +req.params.id;
+        const post: PostViewModel | undefined = posts.find(p => p.id === id)
+        if (post) {
+            post.title = title;
+            post.shortDescription = shortDescription;
+            post.content = content;
+            post.bloggerId = bloggerId;
+            post.bloggerName = bloggers[bloggerId-1].name;
+            res.status(204).send(post)
+        }
+        res.status(404).send('NotFound')
+    }
+    else{
+        const errorMessage: APIErrorResult = {
+            errorsMessages: [{
+                message: "Field title not found",
+                field: "title"
+            }]
+        }
+        res.status(400).send(errorMessage)
+    }
+
+})
+
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`)
+})
