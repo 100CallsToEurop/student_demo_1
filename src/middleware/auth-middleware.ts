5d7964c47b994ef360689fdd960b65dabb461e9c
100CallsToEurop <piaterik123123@mail.ru>
2022-07-14 14:28:10 +0300

    test password

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -2,9 +2,10 @@ import {Request, Response, NextFunction} from "express";
 const auth = {login: 'admin', password: 'qwerty'}
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const typeAuth = (req.headers.authorization || '').split(' ')[0] || ''
     const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
     const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
-    if(login && password && login === auth.login && password === auth.password){
+    if(login && password && login === auth.login && password === auth.password && typeAuth === 'Basic'){
         next();
     }
     else res.status(401).send(401)
