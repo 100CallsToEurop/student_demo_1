64e022cbd7668ac54cd40cd956e5b8b34227e4ce
100CallsToEurop <piaterik123123@mail.ru>
2022-07-15 22:49:36 +0300

    blabal

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,7 +1,7 @@
 import {MongoClient} from "mongodb";
 import {BloggerViewModel, PostViewModel} from "../types";
 /*"mongodb+srv://user:sven171995@cluster0.tuuab.mongodb.net/?retryWrites=true&w=majority"*/
-const uri = "mongodb+srv://piaterik123123@mail.ru:sven171995@cluster0.tuuab.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://piaterik123123%40mail.ru:sven171995@cluster0.tuuab.mongodb.net/?retryWrites=true&w=majority";
 
 
 
