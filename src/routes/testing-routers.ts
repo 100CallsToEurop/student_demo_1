4a81f6a049d853fde9b72e2eea972f95dc9db087
100CallsToEurop <piaterik123123@mail.ru>
2022-07-25 23:46:50 +0300

    new work

--- src/routes/testing-routers.ts
+++ src/routes/testing-routers.ts
@@ -3,11 +3,14 @@ import {bloggersCollection, commentsCollection, postsCollection, usersCollection
 
 export const testingRouter = Router({})
 
-testingRouter.delete('/all-data', async(req: Request, res: Response) =>{
-    await bloggersCollection.deleteMany({})
-    await usersCollection.deleteMany({})
-    await postsCollection.deleteMany({})
-    await commentsCollection.deleteMany({})
-    res.status(204).send('All data is deleted')
-    return
+testingRouter.delete('/all-data', async (req: Request, res: Response) =>{
+    if(await bloggersCollection.deleteMany({}) &&
+    await usersCollection.deleteMany({}) &&
+    await postsCollection.deleteMany({}) &&
+    await commentsCollection.deleteMany({}))
+   {
+        res.status(204).send('All data is deleted')
+        return
+    }
+
 })
