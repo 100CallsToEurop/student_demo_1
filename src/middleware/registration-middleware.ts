a42c543793367f2e2e06011f175a310dc17ea491
100CallsToEurop <piaterik123123@mail.ru>
2022-07-26 20:46:54 +0300

    mailer

--- src/middleware/registration-middleware.ts
+++ src/middleware/registration-middleware.ts
@@ -3,5 +3,4 @@ import {body} from "express-validator";
 export const emailValidationRegistration = body('email')
     .exists()
     .notEmpty()
-    .isEmail()
     .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
